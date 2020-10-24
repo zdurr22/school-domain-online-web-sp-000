4114e5e92b7d729d2cfc56dbde32196a76fb9826
Zachary Durr <zdurr22@gmail.com>
2020-10-24 21:21:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -23,8 +23,11 @@ class School
    roster[number]
  end
 
-def sort
-  @roster.each do |grade, name|
-    @roster[grade] = name.sort
+ def sort
+   @roster.each do |grade, name|
+     @roster[grade] = name.sort
+   end
+ end
+
 
 end
