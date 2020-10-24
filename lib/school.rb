9dcc255872661801c78edadca5e9ac3c2d1bdf8e
Zachary Durr <zdurr22@gmail.com>
2020-10-24 21:14:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,17 @@
 class School
+  roster = {
 
-  def roster
-    @roster = {
+  }
 
-    }
-
-  end
 
   def initialize(name)
     @name = name
+    def roster
+      roster = {
 
+      }
+
+    end
   end
 
  def add_student(name, grade)
