8aa4bd71cde53c0ca2db616ff715881e76047ffe
Zachary Durr <zdurr22@gmail.com>
2020-10-24 21:20:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,16 +9,20 @@ class School
     @roster
   end
 
-
  def add_student(name, grade)
    @student_name = name
    @grade = grade
+
    if roster.include?(grade) == false
       roster[grade] = []
     end
     @roster[grade] << name
  end
 
+ def grade(number)
+   roster[number]
+ end
+ 
 
 
 end
